032287581796c30434de21633413f6662f000f52
aliko <gracealiko09@gmail.com>
2025-02-24 19:40:34 +0000

    decided to use css modules instead

--- worldwise/src/pages/HomePage.jsx
+++ worldwise/src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
-const HomePage = () => {
+import styles from "./Homepage.module.css";
+
+export default function Homepage() {
   return (
-    <main className="h-[calc(100vh-5rem)] m-5 p-10 md:p-20 bg-[linear-gradient(to_bottom,rgba(36,42,46,0.8),rgba(36,42,46,0.8)),url('./assets/images/bg.jpg')] bg-cover bg-center">
-      <section className="flex flex-col h-[85%] items-center justify-center gap-10 text-center">
-        <h1 className="text-5xl leading-snug text-[var(--color-light--1)] ">
+    <main className={styles.homepage}>
+      <section>
+        <h1>
           You travel the world.
           <br />
-          WorldWise keeps track of your adventures
+          WorldWise keeps track of your adventures.
         </h1>
-        <h2 className="w-[90%] mb-10 text-xl text-[var(--color-light--1)]">
+        <h2>
           A world map that tracks your footsteps into every city you can think
           of. Never forget your wonderful experiences, and show your friends how
           you have wandered the world.
@@ -15,6 +17,4 @@ const HomePage = () => {
       </section>
     </main>
   );
-};
-
-export default HomePage;
+}
