63c6781319cbbffec35270e695810ad7e0ab02be
aliko <gracealiko09@gmail.com>
2025-02-26 23:48:33 +0000

    added qeustions

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/Header";
-
+import Main from "./components/Main";
 const App = () => {
+  useEffect(() => {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log("Error"));
+  }, []);
   return (
-    <div>
+    <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 };
