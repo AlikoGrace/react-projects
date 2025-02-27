d2f6ef56b9048d48d60354fa32f3845c56458f30
aliko <gracealiko09@gmail.com>
2025-02-27 00:21:35 +0000

    added useReducer

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,12 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+
+const initialState = {
+  questions: [],
+  //  "loading", "active","error", "active","finished"
+  status: "isLoading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+        // status and questions will change at the same time most of the time, that's why it's good to use useReducer here
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+
+    default:
+      throw new Error("Action unkown");
+  }
+}
+
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
-      .then((data) => console.log(data))
-      .catch((err) => console.log("Error"));
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
   return (
     <div className="app">
