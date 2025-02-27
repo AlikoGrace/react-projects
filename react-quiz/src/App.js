e3d2e54e2b011ed03e1ebf4220d17a3aeaa5c414
aliko <gracealiko09@gmail.com>
2025-02-27 09:03:00 +0000

    added ui for loading and error

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,18 +1,20 @@
 import React, { useEffect, useReducer } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
 
 const initialState = {
   questions: [],
   //  "loading", "active","error", "active","finished"
-  status: "isLoading",
+  status: "loading",
 };
 function reducer(state, action) {
   switch (action.type) {
     case "dataRecieved":
       return {
         ...state,
-        question: action.payload,
+        questions: action.payload,
         status: "ready",
         // status and questions will change at the same time most of the time, that's why it's good to use useReducer here
       };
@@ -28,7 +30,8 @@ function reducer(state, action) {
 }
 
 const App = () => {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  // const [state, dispatch] = useReducer(reducer, initialState); i want to distructure state
+  const [{ question, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(() => {
     fetch("http://localhost:9000/questions")
       .then((res) => res.json())
@@ -39,8 +42,8 @@ const App = () => {
     <div className="app">
       <Header />
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
       </Main>
     </div>
   );
