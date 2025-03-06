54a9c24107042f194c3882c60be23c530f28446a
aliko <gracealiko09@gmail.com>
2025-03-06 19:09:28 +0000

    added the app's layout

---
+++ fast-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <h1>Content</h1>
+      </main>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
