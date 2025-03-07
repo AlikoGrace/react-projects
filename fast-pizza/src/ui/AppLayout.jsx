501ba4d2e10a22db387dc263a9b139dd6405e24d
aliko <gracealiko09@gmail.com>
2025-03-07 00:45:24 +0000

    added errorElement

--- fast-pizza/src/ui/AppLayout.jsx
+++ fast-pizza/src/ui/AppLayout.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import Loader from "./Loader";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const AppLayout = () => {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content</h1>
