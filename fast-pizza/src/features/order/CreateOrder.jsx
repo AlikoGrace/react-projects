b4ad3947dcbdde7da8b7a8754957b373ddc2d865
aliko <gracealiko09@gmail.com>
2025-03-07 15:27:06 +0000

    used action from react router dom for post request

--- fast-pizza/src/features/order/CreateOrder.jsx
+++ fast-pizza/src/features/order/CreateOrder.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { Form } from "react-router-dom";
+import { object } from "yup";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -38,7 +40,7 @@ function CreateOrder() {
     <div>
       <h2>Ready to order? Let's go!</h2>
 
-      <form>
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -72,9 +74,16 @@ function CreateOrder() {
         <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+  return null;
 }
 
 export default CreateOrder;
